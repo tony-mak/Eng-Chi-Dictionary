5f22f2115814c49a24a26aa3c69026389fe98ff0
Tony Mak <ming030890@gmail.com>
2015-04-12 10:34:45 +0100

    Retrieve version string in runtime

--- app/src/main/java/com/madeinhk/app/AboutFragment.java
+++ app/src/main/java/com/madeinhk/app/AboutFragment.java
@@ -1,6 +1,9 @@
 package com.madeinhk.app;
 
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.madeinhk.english_chinesedictionary.R;
 
@@ -22,6 +26,8 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_about, container, false);
+        TextView versionTextView = (TextView) rootView.findViewById(R.id.version);
+        versionTextView.setText("Version " + getAppVersionString());
         Button contactButton = (Button) rootView.findViewById(R.id.contact_button);
         contactButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,5 +39,15 @@ public class AboutFragment extends Fragment {
             }
         });
         return rootView;
+    }
+
+    private String getAppVersionString() {
+        try {
+            PackageInfo packageInfo = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0);
+            return packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return "N/A";
     }
 }
