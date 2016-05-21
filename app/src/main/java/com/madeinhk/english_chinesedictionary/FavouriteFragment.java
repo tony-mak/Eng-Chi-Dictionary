3213408a73386060a5f4e56816890965cbddb756
Tony Mak <ming030890@gmail.com>
2016-05-21 10:01:52 +0100

    Fix array out of bound exception

--- app/src/main/java/com/madeinhk/english_chinesedictionary/FavouriteFragment.java
+++ app/src/main/java/com/madeinhk/english_chinesedictionary/FavouriteFragment.java
@@ -28,7 +28,6 @@ import de.greenrobot.event.EventBus;
 public class FavouriteFragment extends Fragment {
     private RecyclerView mRecyclerView;
     private CardListAdapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
     private View mEmptyView;
 
     public FavouriteFragment() {
@@ -64,75 +63,6 @@ public class FavouriteFragment extends Fragment {
         mRecyclerView.setItemAnimator(new CustomItemAnimator());
 
         return view;
-    }
-
-    public static class MyLinearLayoutManager extends LinearLayoutManager {
-
-        public MyLinearLayoutManager(Context context, int orientation, boolean reverseLayout) {
-            super(context, orientation, reverseLayout);
-        }
-
-        private int[] mMeasuredDimension = new int[2];
-
-        @Override
-        public void onMeasure(RecyclerView.Recycler recycler, RecyclerView.State state,
-                              int widthSpec, int heightSpec) {
-            final int widthMode = View.MeasureSpec.getMode(widthSpec);
-            final int heightMode = View.MeasureSpec.getMode(heightSpec);
-            final int widthSize = View.MeasureSpec.getSize(widthSpec);
-            final int heightSize = View.MeasureSpec.getSize(heightSpec);
-            int width = 0;
-            int height = 0;
-            for (int i = 0; i < state.getItemCount(); i++) {
-                measureScrapChild(recycler, i,
-                        View.MeasureSpec.makeMeasureSpec(i, View.MeasureSpec.UNSPECIFIED),
-                        View.MeasureSpec.makeMeasureSpec(i, View.MeasureSpec.UNSPECIFIED),
-                        mMeasuredDimension);
-
-                if (getOrientation() == HORIZONTAL) {
-                    width = width + mMeasuredDimension[0];
-                    if (i == 0) {
-                        height = mMeasuredDimension[1];
-                    }
-                } else {
-                    height = height + mMeasuredDimension[1];
-                    if (i == 0) {
-                        width = mMeasuredDimension[0];
-                    }
-                }
-            }
-            switch (widthMode) {
-                case View.MeasureSpec.EXACTLY:
-                    width = widthSize;
-                case View.MeasureSpec.AT_MOST:
-                case View.MeasureSpec.UNSPECIFIED:
-            }
-
-            switch (heightMode) {
-                case View.MeasureSpec.EXACTLY:
-                    height = heightSize;
-                case View.MeasureSpec.AT_MOST:
-                case View.MeasureSpec.UNSPECIFIED:
-            }
-
-            setMeasuredDimension(width, height);
-        }
-
-        private void measureScrapChild(RecyclerView.Recycler recycler, int position, int widthSpec,
-                                       int heightSpec, int[] measuredDimension) {
-            View view = recycler.getViewForPosition(position);
-            if (view != null) {
-                RecyclerView.LayoutParams p = (RecyclerView.LayoutParams) view.getLayoutParams();
-                int childWidthSpec = ViewGroup.getChildMeasureSpec(widthSpec,
-                        getPaddingLeft() + getPaddingRight(), p.width);
-                int childHeightSpec = ViewGroup.getChildMeasureSpec(heightSpec,
-                        getPaddingTop() + getPaddingBottom(), p.height);
-                view.measure(childWidthSpec, childHeightSpec);
-                measuredDimension[0] = view.getMeasuredWidth() + p.leftMargin + p.rightMargin;
-                measuredDimension[1] = view.getMeasuredHeight() + p.bottomMargin + p.topMargin;
-                recycler.recycleView(view);
-            }
-        }
     }
 
     private class UpdateFavTask extends AsyncTask<Void, Void, List<Word>> {
@@ -246,7 +176,7 @@ public class FavouriteFragment extends Fragment {
             holder.mRecycleView.setHasFixedSize(true);
             holder.mRecycleView.setAdapter(new WordListAdapter(mSections.get(position)));
 
-            MyLinearLayoutManager mgr = new MyLinearLayoutManager(mContext, LinearLayoutManager.VERTICAL, false);
+            LinearLayoutManager mgr = new LinearLayoutManager(mContext);
             holder.mRecycleView.setLayoutManager(mgr);
         }
 
@@ -339,7 +269,7 @@ public class FavouriteFragment extends Fragment {
             Collections.sort(words, new Comparator<Word>() {
                 @Override
                 public int compare(Word lhs, Word rhs) {
-                    return lhs.mWord.compareTo(rhs.mWord);
+                    return lhs.mWord.compareToIgnoreCase(rhs.mWord);
                 }
             });
             return words;
